0ee6703aca98572ab7f3d4734e311529fa1ab8fd
diego.cruz <diego.cruz@accountfy.com>
2021-10-18 21:40:15 -0300

    faltando duas tabelas

---
+++ EndorfinaSegundoSemestre/src/br/com/fiap/tds/bo/UsuarioProfissionalBO.java
@@ -0,0 +1,51 @@
+package br.com.fiap.tds.bo;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.fiap.tds.bean.Usuario;
+import br.com.fiap.tds.bean.UsuarioProfissional;
+import br.com.fiap.tds.dao.UsuarioProfissionalDAO;
+import br.com.fiap.tds.exception.DadosInvalidosException;
+import br.com.fiap.tds.exception.IdNotFoundException;
+
+public class UsuarioProfissionalBO {
+	
+	private UsuarioProfissionalDAO usuarioProfissionalDAO;
+	private UsuarioBO usuarioBO;
+	private ProfissionalBO profissionalBO;
+	private Connection conexao;
+
+
+	public UsuarioProfissionalBO(Connection conexao) {
+		this.conexao = conexao;
+		this.usuarioProfissionalDAO = new UsuarioProfissionalDAO(conexao);
+		this.profissionalBO = new ProfissionalBO(conexao);
+		this.usuarioBO = new UsuarioBO(conexao);
+		
+	}
+	
+	public void cadastrarUsuario(UsuarioProfissional usuarioProfissional) throws SQLException, DadosInvalidosException{
+		conexao.setAutoCommit(false);
+		usuarioBO.cadastrar(usuarioProfissional.getUsuario());
+		profissionalBO.cadastrar(usuarioProfissional.getProfissional());		
+		usuarioProfissionalDAO.cadastrar(usuarioProfissional);
+		
+		try {
+			conexao.commit();
+		}catch(Exception e) {
+			conexao.rollback();
+			throw new SQLException("Erro ao realizar commit");
+		}
+	}
+	
+	public UsuarioProfissional pesquisar(int codigoUsuario, int codigoProfissional) throws SQLException, IdNotFoundException{
+		return usuarioProfissionalDAO.pesquisar(codigoUsuario, codigoProfissional);
+	}
+	
+	public void remover(int codigo) throws SQLException, IdNotFoundException{
+		usuarioProfissionalDAO.remover(codigo);
+	}
+	
+	
+}
--- EndorfinaSegundoSemestre/src/br/com/fiap/tds/dao/UsuarioProfissionalDAO.java
+++ EndorfinaSegundoSemestre/src/br/com/fiap/tds/dao/UsuarioProfissionalDAO.java
@@ -2,9 +2,14 @@ package br.com.fiap.tds.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import br.com.fiap.tds.bean.Usuario;
 import br.com.fiap.tds.bean.UsuarioProfissional;
+import br.com.fiap.tds.exception.IdNotFoundException;
 
 public class UsuarioProfissionalDAO {
 
@@ -15,29 +20,78 @@ public class UsuarioProfissionalDAO {
 		
 	}
 
-	public void cadastrarUsuario(UsuarioProfissional usuarioProfissional) throws SQLException {
+	public void cadastrar(UsuarioProfissional usuarioProfissional) throws SQLException {
 		//Criar o comando SQL
 		PreparedStatement stmt = conexao.prepareStatement("INSERT INTO ENDORF_USUARIO_PROFI "
 				+ "(ID_USUARIO, ID_PROFISSIONAL)"
 				+ " VALUES (?, ?)");
 		
 		//passar os valores para o comando SQL
-		stmt.setInt(1, usuarioProfissional.getIdUsuario());
-		stmt.setInt(2, usuarioProfissional.getIdProfissional());
+		stmt.setInt(1, usuarioProfissional.getProfissional().getIdProfissional());
+		stmt.setInt(2, usuarioProfissional.getUsuario().getIdUsuario());
 		
 		//Executar o comando SQL
 		stmt.executeUpdate();
 	}
 	
-	
-		//Lista e remover
-	
-		public void atualizar(UsuarioProfissional usuarioProfissional) {
+	public UsuarioProfissional pesquisar(int codigoUsuario, int codigoProfissional) throws SQLException, IdNotFoundException{
+		
+		PreparedStatement stmt = conexao.prepareStatement("SELECT * FROM ENDORF_USUARIO WHERE ID_USUARIO = ? AND ID_PROFISSIONAL = ?");
+		
+		stmt.setInt(1, codigoUsuario);	
+		stmt.setInt(2, codigoProfissional);		
+
+
+		ResultSet result = stmt.executeQuery();
+
+		if(!result.next()) {
+			throw new IdNotFoundException("Usuario/Profissional não encontrado");
 			
 		}
 		
-		public UsuarioProfissional pesquisar(int codigo) {
-			return null;
+		UsuarioProfissional usuarioProfissional = parse(result);
+		
+		return usuarioProfissional;
+	}
+	
+	public List<UsuarioProfissional> listar() throws SQLException{
+		
+		PreparedStatement stmt = conexao.prepareStatement("SELECT * FROM ENDORF_USUARIO_PROFI");
+		
+		ResultSet result = stmt.executeQuery();
+
+		List<UsuarioProfissional> lista = new ArrayList<UsuarioProfissional>();
+
+		while(result.next()) {			
+			UsuarioProfissional usuarioProfissional = parse(result);
+			lista.add(usuarioProfissional);
 		}
+		return lista;
+	}
+	
+	private UsuarioProfissional parse(ResultSet result) throws SQLException {
+
+		int idUsuario = result.getInt("ID_USUARIO");
+		int idProfissional = result.getInt("ID_PROFISSIONAL");		
+		
+		UsuarioProfissional usuarioProfissional = null;
+		usuarioProfissional.getUsuario().setIdUsuario(idUsuario);
+		usuarioProfissional.getProfissional().setIdProfissional(idProfissional);
+		
+		return usuarioProfissional;			
+	}
+	
+	public void remover(int codigoUsuario) throws SQLException, IdNotFoundException{
+		
+		PreparedStatement stmt = conexao.prepareStatement("DELETE FROM ENDORF_USUARIO_PROFI WHERE ID_USUARIO = ?");
+
+		stmt.setInt(1, codigoUsuario);	
+		
+		int qtd = stmt.executeUpdate();
+
+		if(qtd == 0) {
+			throw new IdNotFoundException("Usuario/Profissional não encontrado para ser removido");
+		}
+	}
 	
 }
